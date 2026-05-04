aabd3056426f4f4760f68a82ae14f40e61387a3d
princerathore981 <prince9761462201@gmail.com>
2026-05-04 11:43:49 +0530

    Update Bisection Method.py
    Added a new function to find the root of a cubic equation using the bisection method.

--- Bisection Method.py
+++ Bisection Method.py
@@ -1,14 +1,17 @@
-def bisection(f, a, b, tol=1e-6, max_iter=100):
-    if f(a) * f(b) >= 0:
-        print("Invalid interval")
-        return None
+def f(x):
+    return x**3 - x - 2
 
-    for i in range(max_iter):
-        c = (a + b) / 2
-        if abs(f(c)) < tol:
-            return c
-        elif f(a) * f(c) < 0:
-            b = c
-        else:
-            a = c
-    return c
+a = 1
+b = 2
+tol = 0.0001
+
+while (b - a) / 2 > tol:
+    c = (a + b) / 2
+    if f(c) == 0:
+        break
+    elif f(a) * f(c) < 0:
+        b = c
+    else:
+        a = c
+
+print("Root:", c)
