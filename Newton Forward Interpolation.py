dacce0528dcd8e596b50c8208826703e456f4bcc
princerathore981 <prince9761462201@gmail.com>
2026-05-04 11:47:07 +0530

    Update Newton Forward Interpolation.py
    Refactored Newton Forward Interpolation to use NumPy for difference table calculation and improved readability.

--- Newton Forward Interpolation.py
+++ Newton Forward Interpolation.py
@@ -1,23 +1,25 @@
-def newton_forward(x, y, xp):
-    n = len(x)
-    diff = [y.copy()]
+import numpy as np
 
-    for i in range(1, n):
-        temp = []
-        for j in range(n - i):
-            temp.append(diff[i-1][j+1] - diff[i-1][j])
-        diff.append(temp)
+x = [0, 1, 2, 3]
+y = [1, 2, 4, 8]
 
-    h = x[1] - x[0]
-    p = (xp - x[0]) / h
+n = len(x)
+diff = np.zeros((n, n))
+diff[:,0] = y
 
-    result = y[0]
-    fact = 1
-    p_term = 1
+for j in range(1, n):
+    for i in range(n-j):
+        diff[i][j] = diff[i+1][j-1] - diff[i][j-1]
 
-    for i in range(1, n):
-        fact *= i
-        p_term *= (p - i + 1)
-        result += (p_term * diff[i][0]) / fact
+value = 1.5
+h = x[1] - x[0]
+p = (value - x[0]) / h
 
-    return result
+result = y[0]
+term = 1
+
+for i in range(1, n):
+    term *= (p - i + 1) / i
+    result += term * diff[0][i]
+
+print("Interpolated value:", result)
